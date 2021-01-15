792efee5ae8759ffa194d1e628d845cd7468498c
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-16 02:24:50 +0800

    Plant Details UI - Maps #2

--- app/src/main/java/com/zystems/plantdex/PlantDetailsActivity.java
+++ app/src/main/java/com/zystems/plantdex/PlantDetailsActivity.java
@@ -1,11 +1,12 @@
 package com.zystems.plantdex;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ScrollView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,11 +14,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.zystems.plantdex.adapters.CustomMapFragment;
 
 public class PlantDetailsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private ImageButton btnMap, btnBack;
     private GoogleMap googleMap;
+    private ScrollView scrollContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +29,8 @@ public class PlantDetailsActivity extends FragmentActivity implements OnMapReady
 
         btnBack = (ImageButton) findViewById(R.id.btnBack);
         btnMap = (ImageButton) findViewById(R.id.btnMap);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        CustomMapFragment mapFragment = (CustomMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        scrollContainer = (ScrollView) findViewById(R.id.scrollContainer);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +40,7 @@ public class PlantDetailsActivity extends FragmentActivity implements OnMapReady
         });
 
         mapFragment.getMapAsync(this);
+
     }
 
     @Override
@@ -47,5 +52,34 @@ public class PlantDetailsActivity extends FragmentActivity implements OnMapReady
                 .position(sydney)
                 .title("Marker in Sydney"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        ((CustomMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                .setListener(new CustomMapFragment.OnTouchListener() {
+                    @Override
+                    public void onTouch()
+                    {
+                        scrollContainer.requestDisallowInterceptTouchEvent(true);
+                    }
+                });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                // Disallow ScrollView to intercept touch events.
+                scrollContainer.requestDisallowInterceptTouchEvent(true);
+                break;
+
+            case MotionEvent.ACTION_UP:
+                // Allow ScrollView to intercept touch events.
+                scrollContainer.requestDisallowInterceptTouchEvent(false);
+                break;
+        }
+
+        // Handle MapView's touch events.
+        super.onTouchEvent(event);
+        return true;
     }
 }
