9051100a230b7d2564bb3498d9fd5529c21cae21
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-16 03:07:57 +0800

    Capture & Classify UI

--- app/src/main/java/com/zystems/plantdex/CaptureActivity.java
+++ app/src/main/java/com/zystems/plantdex/CaptureActivity.java
@@ -10,5 +10,6 @@ public class CaptureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture);
+
     }
 }
