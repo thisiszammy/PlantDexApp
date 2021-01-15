3106b7866166fc9b8566dd2baf39dd43edd842e9
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-01-15 15:59:51 +0800

    Search Result Card Layout

--- app/src/main/java/com/zystems/plantdex/SearchActivity.java
+++ app/src/main/java/com/zystems/plantdex/SearchActivity.java
@@ -2,21 +2,28 @@ package com.zystems.plantdex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class SearchActivity extends AppCompatActivity {
 
     private ImageButton btnBack;
+    private EditText txtPlantName;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
         btnBack = (ImageButton) findViewById(R.id.btnBack);
+        txtPlantName = (EditText) findViewById(R.id.txtPlantName);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +32,24 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
+        txtPlantName.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
 
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (txtPlantName.getRight() - txtPlantName.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+
+                        Toast.makeText(getApplicationContext(), txtPlantName.getText().toString(), Toast.LENGTH_LONG).show();
+
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/zystems/plantdex/adapters/SearchPlantAdapter.java
@@ -0,0 +1,18 @@
+package com.zystems.plantdex.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SearchPlantAdapter {
+
+    class PlantViewHolder extends RecyclerView.ViewHolder {
+
+
+        public PlantViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+    }
+}
